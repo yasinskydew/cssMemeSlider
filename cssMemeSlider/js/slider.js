b558cdeabab4115a5b37bb5af43624963ab24d3f
yasinskyvladimir <uladzimir.yasinski@twinslash.com>
2023-09-17 19:19:54 +0300

    feat: add js slider logic (Sun Sep 7 2023 19:50:47)

--- cssMemeSlider/js/slider.js
+++ cssMemeSlider/js/slider.js
@@ -1,30 +1,46 @@
 export default class Slider {
-  constructor(){};
-
-  carouselWrapper = document.querySelector('.about-slider-wrapper');
-  carousel = document.querySelector('.about-slider');
-  
-  sliderButtonList = document.querySelector('.slider-button-list');
-  sliderButtonCollection = this.sliderButtonList.querySelectorAll('.slider-button');
-  
-  itemWidth = this.carouselWrapper.querySelector('.about-slider-item').clientWidth + 22;
-  
-  currentSlideIndex = 0;
-
-  sliderDuration = {
-      duration: 200,
-      iterations: 1,
-      fill: 'forwards',
+  constructor(data) {
+    this.data = data;
+    this.carouselWrapper = document.querySelector('.slider-wrapper');
+    this.carouselWrapper = document.querySelector('.slider-wrapper');
+    this.carousel = document.querySelector('.slider');
+    this.description = document.querySelector('.slide-description');
+    this.sliderButtonList = document.querySelector('.slider-button-list');
+    this.sliderButtonCollection = [];
+    this.itemWidth = 0;
+    this.currentSlideIndex = 0;
+    this.sliderDuration = {
+        duration: 200,
+        iterations: 1,
+        fill: 'forwards',
+    };
   };
 
+  _logData() {
+    console.log(this.data);
+  }
+
   _sliderAnimation = (index) => {
-    console.log(this.itemWidth);
     if(index === this.currentSlideIndex) return false;
     return [
       {
         transform: `translateX(-${this.itemWidth * index}px)`
       }
     ]
+  }
+
+  _descriptionAnimation = (index) => {
+    if(index === this.currentSlideIndex) return false;
+    return {
+      opacity: [0, 1],
+    }
+  }
+
+  _initDescription() {
+    const value = this.data[this.currentSlideIndex].description;
+    this.description.innerText = value;
+    const animation = this._descriptionAnimation();
+    this.description.animate(animation, this.sliderDuration);
   }
 
   _sliderAnimationClick = (index) => {
@@ -35,17 +51,56 @@ export default class Slider {
       this.sliderButtonCollection.forEach(i => i.classList.remove('slider-button__active'));
       this.sliderButtonCollection[index].classList.add('slider-button__active');
     }
+    this._initDescription();
+  }
+
+  _initSlides() {
+    const slides = this.data.map((i) => {
+      const el = document.createElement('li');
+      el.classList.add('slider-item');
+      if (i.isActive) {
+        el.classList.add('slider-item__active');
+      }
+      const img = document.createElement('img');
+      img.classList.add('slider-image');
+      img.setAttribute('alt', 'slider item');
+      img.setAttribute('src', i.link);
+
+      el.appendChild(img);
+      return el;
+    });
+    this.carousel.replaceChildren(...slides);
+    this.itemWidth = slides[0].clientWidth + 22;
+  }
+
+  _initButtons() {
+    const buttons = this.data.map((i, index) => {
+      const el = document.createElement('li');
+      el.classList.add('slider-button');
+      if (i.isActive) {
+        el.classList.add('slider-button__active');
+      }
+      const span = document.createElement('span');
+      span.classList.add('visually-hidden');
+      span.innerText = `${index + 1}`;
+      el.addEventListener('click', () => {
+        this._sliderAnimationClick(index);
+      })
+      el.appendChild(span);
+      return el;
+    });
+    this.sliderButtonCollection = buttons;
+    this.sliderButtonList.replaceChildren(...buttons);
   }
 
   _renderSlider() {
-    this.sliderButtonCollection.forEach((item, index) => {
-      item.addEventListener('click', () => {
-        this._sliderAnimationClick(index);
-      })
-    });
+    this._initSlides();
+    this._initButtons();
+    this._initDescription();
   }
 
   init(){
       this._renderSlider();
+      this._logData();
   }
 }
